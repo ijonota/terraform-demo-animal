7c28550fbf14947c44a698c09fbf092486f60865
ijonota <ivanjonota@discover.com>
2018-08-08 08:01:48 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,10 @@
- output "name" {
-   value       = "${random_pet.animal.id}"
-   description = "Contains the name of a random animal."
- }
+output "name" {
+  value       = "${random_pet.animal.id}"
+  description = "Contains the name of a random animal."
+}
+
+output "module_name" {
+  value       = ""${module.animal.name}""
+  description = "Contains the name of this module."
+}
+
